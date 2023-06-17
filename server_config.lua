34ab9ac6efe17bc0e5806cb959db95a353ce4e4c
Defeatss <107814631+Defeatss@users.noreply.github.com>
2023-06-17 13:32:48 +0200

    Add files via upload

---
+++ server_config.lua
@@ -0,0 +1,2 @@
+steamApiKey = "CF6D42A34749A0CC6171E7694E0237F8"
+Discord_Webhook = "https://discord.com/api/webhooks/1117804955663867975/u3MUc0CffyvrRX5U3gEauyg5yrGBfhOG2OR0mDc8Lp-FPuKW6TeqCWhm007uCaSyuE05"
