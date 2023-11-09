c87aa771d9b77901ba4120b5f8fbfdf04e78e478
Defeatss <107814631+Defeatss@users.noreply.github.com>
2023-11-09 11:27:23 +0100

    Update server_config.lua

--- server_config.lua
+++ server_config.lua
@@ -1,2 +1,2 @@
-steamApiKey = "CF6D42A34749A0CC6171E7694E0237F8"
-Discord_Webhook = "https://discord.com/api/webhooks/1117804955663867975/u3MUc0CffyvrRX5U3gEauyg5yrGBfhOG2OR0mDc8Lp-FPuKW6TeqCWhm007uCaSyuE05"
+steamApiKey = "YOUR_STEAMAPIKEY"
+Discord_Webhook = "YOUR_DISCORD_WEBHOOK"
